7f935d2104801230023d6d3b117d4e7e06d6675c
nora! <117397809+pellpk@users.noreply.github.com>
2024-09-05 15:56:24 +0100

    source code

---
+++ notepad.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workbook1
+{
+    internal class notepad
+    {
+        static void Main(string[] args)
+        {
+            const string endMessage = "write \"end\" to close the program.";
+            const string clearMessage = "write \"clear\" to clear the program.";
+
+            void PrintInstructions()
+            {
+                Console.WriteLine(endMessage);
+                Console.WriteLine(clearMessage);
+                Console.WriteLine();
+            }
+
+            PrintInstructions();
+
+            while (true)
+            {
+                string response = Console.ReadLine()?.ToLower();
+                if (response == "end")
+                {
+                    return;
+                }
+                if (response == "clear")
+                {
+                    Console.Clear();
+                    PrintInstructions();
+                }
+            }
+        }
+    }
+}
